7a23b295da408daa1f566c11f57760944ea80865
huytranminhcs0707-lab <huy.tranminhcs0707@hcmut.edu.vn>
2025-11-21 20:42:26 +0700

    linhtinh

--- structure_algorithm/linhtinh.cpp
+++ structure_algorithm/linhtinh.cpp
@@ -5,15 +5,7 @@ bool cmp(string a, string b){
   else return a < b;
 }
 int main(){
-  int a[10] ={1, 2, 5, 6, 7, 8, 9, 11, 15, 16};
-  int target = 15;
-  int k = 0;
-  int b;
-  for ( b = 10 / 2; b>=1; b /= 2){
-    while (k+b < 10 && a[k+b] <= target){ k+=b;}
-    
-  }
-  cout << k+b;
+  cout << "Hello World! ";
   
   return 0;
 }
